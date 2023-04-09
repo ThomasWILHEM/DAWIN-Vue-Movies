9b0812f01c4a51a9602a8e8ac03b32b50ff61c05
ThomasWILHEM <thomaswilhempro@outlook.fr>
2023-04-09 19:50:32 +0200

    feat: Movie details added

---
+++ src/components/Pages/MovieDetails.vue
@@ -0,0 +1,22 @@
+<template>
+    <h2>{{ movie.title }}</h2>
+</template>
+
+<script>
+export default {
+    name: "MovieDetails",
+    inject: ['movies'],
+    data() {
+        return {
+            movie: []
+        };
+    },
+    created() {
+        this.movie = this.movies.filter(movie => movie.id == this.$route.params.id)[0];
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 import MovieList from "@/components/Pages/MovieList.vue";
+import MovieDetails from "@/components/Pages/MovieDetails.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: MovieList },
+        {path: '/', component: MovieList},
+        {path: '/movie/:id', component: MovieDetails},
     ],
 
 });
