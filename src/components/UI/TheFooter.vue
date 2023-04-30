adf6d3b7e9c0b011a626b33f16b40908fadf8c7d
ThomasWILHEM <thomaswilhempro@outlook.fr>
2023-04-30 20:31:32 +0200

    style: footer fixed

--- src/components/UI/TheFooter.vue
+++ src/components/UI/TheFooter.vue
@@ -10,14 +10,17 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 .footer {
-    position: fixed;
-    left: 0;
+    display: flex;
+    align-content: center;
+    justify-content: center;
     bottom: 0;
+    left: 0;
     width: 100%;
     background-color: #33373e;
     color: #b5a068;
     text-align: center;
 }
+
 </style>
